c26af6832381ced143c44d40ead0e15756f312d7
unknown <anh.nguyen@rainmaker-labs.com>
2016-06-17 00:12:55 +0700

    first

---
+++ app/src/main/java/com/quanganhct/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.quanganhct.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by quanganh.nguyen on 6/16/2016.
+ */
+public class MainActivity extends Activity {
+    private View mainScreen;
+    private TextView txtCount, txtQuestion;
+    private boolean gameOn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+        this.mainScreen = this.findViewById(R.id.main_screen);
+        this.txtCount = (TextView) this.findViewById(R.id.txt_count);
+        this.txtQuestion = (TextView) this.findViewById(R.id.txt_demand);
+
+        this.mainScreen.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                handleEvent(v, event);
+                return true;
+            }
+        });
+    }
+
+    private void handleEvent(View view, MotionEvent event) {
+        int count = event.getPointerCount();
+        if (count > 1) {
+            this.txtCount.setText(String.format("%d fingers", count));
+        } else {
+            this.txtCount.setText("");
+        }
+    }
+}
---
+++ app/src/main/java/com/quanganhct/myapplication/MyDialog.java
@@ -0,0 +1,16 @@
+package com.quanganhct.myapplication;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+/**
+ * Created by quanganh.nguyen on 6/16/2016.
+ */
+public class MyDialog extends AlertDialog {
+
+    protected MyDialog(Context context) {
+        super(context);
+    }
+
+
+}
